ef994a47018b6dd2be5bf8bd220d617123d6f9e4
Maazans31126 <maazmateen111@gmail.com>
2023-09-16 21:37:13 +0530

    started media query

---
+++ index.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    var navlist = document.getElementById("nav-list");
+    
+    if (nav-list.style.display === "block") {
+        navlist.style.display = "none";
+    } else {
+        navlist.style.display = "block";
+    }
+  }
