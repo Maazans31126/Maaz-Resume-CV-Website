311db71e1500c7974fc7f776f9b0ac7bf214ece5
Maazans31126 <maazmateen111@gmail.com>
2023-09-20 02:44:25 +0530

    strated to create project popup wondows

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
-function showCodeangoProject() {
-    var popup = document.getElementById("popup-content");
-    popup.classList.toggle("show");
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("img-ss");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
   }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" img-ss:hover", "");
+  }
+  x[slideIndex-1].style.display = "block";
+  
+  dots[slideIndex-1].className += " img-ss:hover";
+}
